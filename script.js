d5b3f6e437420e8f427d3bb9b236615a28da0e86
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-10-06 13:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.querySelectorAll('.draggable').forEach(item => {
     item.addEventListener('drop', event => {
         event.preventDefault();
         if (draggedElement !== item) {
-            // Swap the content of the dragged element and the target element
+            // Swap IDs
             const tempId = item.id; // Store the ID of the target element
             item.id = draggedElement.id; // Set target's ID to dragged element's ID
             draggedElement.id = tempId; // Set dragged element's ID to target's ID
